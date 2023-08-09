8214d42f567037cbcaa7bba4b56555901ca64806
Chethan R G <chethanrg37@gmail.com>
2023-08-09 17:50:57 +0530

    paser convert string to number

--- main.zig
+++ main.zig
@@ -1,9 +1,12 @@
 const print = @import("std").debug.print;
 const rightShift = @import("./Rshift.zig");
+const parser = @import("./parser.zig");
 
-pub fn main() void {
+pub fn main() !void {
     const greeitng = "Hello World";
     print("{s}\n", .{greeitng});
     const c = rightShift.rShift(1, 2);
     print("{}\n", .{c});
+    const a = parser.parser("1234560", 10) catch |err| return err;
+    print("{}\n", .{a});
 }
---
+++ parser.zig
@@ -0,0 +1,25 @@
+const maxInt = @import("std").math.maxInt;
+
+pub fn parser(buf: []const u8, radix: u8) !u64 {
+    var x: u64 = 0;
+    for (buf) |char| {
+        const digit = charToDigit(char);
+
+        if (digit >= radix) {
+            return error.InvalidChar;
+        }
+
+        if (@mulWithOverflow(u64, x, radix, &x)) return error.OverFlow;
+        if (@addWithOverflow(u64, x, digit, &x)) return error.OverFlow;
+    }
+    return x;
+}
+
+fn charToDigit(c: u8) u8 {
+    return switch (c) {
+        '0'...'9' => c - '0',
+        'a'...'z' => c - 'a' + 10,
+        'A'...'Z' => c - 'A' + 10,
+        else => maxInt(u8),
+    };
+}
